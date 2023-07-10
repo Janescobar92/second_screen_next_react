eb6cb84297eff94a41d121ec9f14103643a80bb0
Jan Escobar <jescobar@aurgi.com>
2023-07-10 12:59:22 +0200

    [SETTINGS]: LAYOUT

--- app/components/BackGround/BackGround.tsx
+++ app/components/BackGround/BackGround.tsx
@@ -3,25 +3,30 @@ import { styled } from "@mui/material";
 import { AppThemeProvider } from "@/app/providers";
 
 import useCompanyAssets from "@/app/Hooks/useCompanyAssets";
+import { usePathname } from "next/navigation";
+import { ROUTES } from "@/app/constants";
 
 /**
  * StyledBackground is a styled component that represents the background element.
  * It uses the provided image URL as the background image.
  */
 const StyledBackground = styled("div", {
-  shouldForwardProp: (prop) => prop !== "imageUrl",
-})<{ imageUrl: string }>(({ imageUrl }) => ({
-  position: "fixed",
-  top: 0,
-  left: 0,
-  width: "100vw",
-  height: "100vh",
-  backgroundSize: "cover",
-  backgroundPosition: "center",
-  pointerEvents: "none",
-  zIndex: -99999,
-  backgroundImage: `url(${imageUrl})`,
-}));
+  shouldForwardProp: (prop) => prop !== "imageUrl" && prop !== "blurBackground",
+})<{ imageUrl: string; blurBackground: boolean }>(
+  ({ imageUrl, blurBackground }) => ({
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100vw",
+    height: "100vh",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    pointerEvents: "none",
+    zIndex: -99999,
+    backgroundImage: `url(${imageUrl})`,
+    filter: blurBackground ? "blur(0.3rem)" : "blur(0px)",
+  })
+);
 
 /**
  * Background is a React component that renders the background element.
@@ -30,10 +35,14 @@ const StyledBackground = styled("div", {
  */
 function Background(): JSX.Element {
   const { backgroundImage } = useCompanyAssets();
+  const pathname = usePathname();
 
   return (
     <AppThemeProvider>
-      <StyledBackground imageUrl={backgroundImage} />
+      <StyledBackground
+        imageUrl={backgroundImage}
+        blurBackground={pathname === ROUTES.settings}
+      />
     </AppThemeProvider>
   );
 }
